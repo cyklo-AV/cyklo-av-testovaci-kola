5a44c07d3e214d959ee1951fd00cc71e39eefa60
cyklo-AV <cyklo-av@volny.cz>
2026-04-12 12:03:27 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -102,9 +102,8 @@ export default function App() {
   ))}
 </select>
 
-      <input type="number" name="price" placeholder="Cena/den" onChange={handleChange} />
-      <input type="number" name="days" placeholder="Dny" onChange={handleChange} />
-      <input type="number" name="deposit" placeholder="Kauce" onChange={handleChange} />
+      <input type="number" value={form.price} readOnly />
+<input type="number" value={form.deposit} readOnly />
 
       <h3>Doplňky</h3>
       {EXTRAS.map(e => (
